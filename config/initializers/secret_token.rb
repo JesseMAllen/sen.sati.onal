1c5df3555f1500a7bfe1ec8a8226374b7009288b
Jesse McGil Allen <jmallen7@gmail.com>
2012-09-28 17:50:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SenSatiOnal::Application.config.secret_token = 'ee3aeaf0f429f64d0f3c975ce311eb95f9e0395a8a253285b4a768e732f9dfc2ddfb6fed17e187ddc431b7109ba10c733d4d9452ff031e5ac4fbb9433a7f1263'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SenSatiOnal::Application.config.session_store :cookie_store, key: '_sen.sati.onal_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SenSatiOnal::Application.config.session_store :active_record_store
