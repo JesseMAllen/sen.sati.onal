7ea22fdcfede8b38e10aa95501a97656b0f1a465
Jesse McGil Allen <jmallen7@gmail.com>
2012-10-06 14:43:47 -0500

    Added links Test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,4 +11,15 @@ describe "StaticPages" do
      it { should have_selector('title', text: full_title('')) }
       it { should_not have_selector 'title', text: '| Home' }
   end
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "Add"
+    page.should have_selector 'title', text: full_title('Create')
+    click_link "View"
+    page.should have_selector 'title', text: full_title('View Bookmarks')
+    click_link "Home"
+    click_link "sen.sati.onal"
+    page.should have_selector 'title', text: full_title('')
+  end
 end
